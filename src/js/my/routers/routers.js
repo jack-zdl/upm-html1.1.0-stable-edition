1c6c72babf02843c22ed840f348decc934cf85ba
jack-zdl <zhangdelei@bsgchina.com>
2017-05-05 11:11:52 +0800

    ms web 测试

--- src/js/my/routers/routers.js
+++ src/js/my/routers/routers.js
@@ -1,24 +1,21 @@
 /**
  * @Date:   2017-04-21T12:06:39+08:00
- * @Last modified time: 2017-05-03T09:38:55+08:00
+ * @Last modified time: 2017-05-05T11:10:03+08:00
  */
 
 var app = require("../../config.js");
-var dashboardMain = require("../../main/dashboardMain.js");
-var areaMain = require("../../main/areasMain.js");
+
 var Backbone = require('backbone');
+
+
 app.appModel.Routers.main = Backbone.Router.extend({
   //hash maps for Routers
   routes: {
-    "ajax/dashboard.html": "dashboard",
-    "ajax/resources/area.html": "area"
+    "ajax/select.html": "select"
   },
-  dashboard: function() {
+  select: function() {
 
-    dashboardMain();
-  },
-  area: function() {
-    areaMain();
+
   }
 
 });
